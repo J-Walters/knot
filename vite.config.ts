647711b3bff5195c11dd5d98a62ef0220305bbda
Jordan Walters <jwalters012@gmail.com>
2025-09-03 13:20:44 -0400

    config: output site to docs for gihub pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
   base: '/knot/',
+  build: {
+    outDir: 'docs',     // 👈 puts built files in /docs so GitHub Pages can serve them
+  }
 })
